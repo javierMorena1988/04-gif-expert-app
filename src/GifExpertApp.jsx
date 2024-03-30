63b68f50495fa25b3941293a4f5ab45c8cb9af7a
Javier Morena <javiermorenavideo@gmail.com>
2024-03-30 14:02:55 +0100

    remove comments

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,26 +5,17 @@ export const GifExpertApp = () => {
   const [categories, setCategories] = React.useState(["One punch"]);
 
   const onAddCategory = (newCategory) => {
-    // categories.push('valorant')
     if (categories.includes(newCategory)) return;
     setCategories([newCategory, ...categories]);
-    // setCategories( cat => [...cat, 'Valorant'])
   };
 
   return (
     <>
       <h1>GifExpertApp</h1>
-      <AddCategory
-        // setCategories={setCategories}
-        onNewCategory={(event) => onAddCategory(event)}
-      />
+      <AddCategory onNewCategory={(event) => onAddCategory(event)} />
 
       {categories.map((category) => (
         <GifGrid key={category} category={category} />
-        // <div key={category}>
-        //   <h3> {category} </h3>
-        //   <li> {category} </li>
-        // </div>
       ))}
     </>
   );
