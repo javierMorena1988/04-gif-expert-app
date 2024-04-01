831e2fc92089a218e60922187057b94930499cb8
Javier Morena <javiermorenavideo@gmail.com>
2024-04-01 13:12:28 +0200

    simular submit del formulario

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,35 +1,35 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react'
+import PropTypes from 'prop-types'
 
 export const AddCategory = ({ onNewCategory }) => {
-  const [inputValue, setInputValue] = React.useState();
+    const [inputValue, setInputValue] = React.useState()
 
-  const onInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
+    const onInputChange = (event) => {
+        setInputValue(event.target.value)
+    }
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    const newInputValue = inputValue.trim();
-    if (newInputValue.length <= 1) return;
+    const onSubmit = (event) => {
+        event.preventDefault()
+        const newInputValue = inputValue.trim()
+        if (newInputValue.length <= 1) return
 
-    onNewCategory(newInputValue);
-    // setCategories((categories) => [inputValue, ...categories]);
-    setInputValue("");
-  };
+        onNewCategory(newInputValue)
+        // setCategories((categories) => [inputValue, ...categories]);
+        setInputValue('')
+    }
 
-  return (
-    <form onSubmit={onSubmit}>
-      <input
-        type="text"
-        placeholder="buscar gifs"
-        value={inputValue}
-        onChange={onInputChange}
-      />
-    </form>
-  );
-};
+    return (
+        <form onSubmit={onSubmit} aria-label="form">
+            <input
+                type="text"
+                placeholder="buscar gifs"
+                value={inputValue}
+                onChange={onInputChange}
+            />
+        </form>
+    )
+}
 
 AddCategory.propTypes = {
-  onNewCategory: PropTypes.func.isRequired,
-};
+    onNewCategory: PropTypes.func.isRequired,
+}
--- test/components/AddCategory.test.jsx
+++ test/components/AddCategory.test.jsx
@@ -1,13 +1,28 @@
 /* eslint-disable no-undef */
 
-import { fireEvent, render, screen } from "@testing-library/react";
-import { AddCategory } from "../../src/components";
+import { fireEvent, render, screen } from '@testing-library/react'
+import { AddCategory } from '../../src/components'
 
-describe("Pruebas en AddCategory", () => {
-  test("Debe cambiar el valor de la caja de texto", () => {
-    render(<AddCategory onNewCategory={() => []} />);
-    const input = screen.getByRole("textbox");
-    fireEvent.input(input, { target: { value: "Saitama" } }); // aqui es donde el usuario ingresa valores
-    expect(input.value).toBe("Saitama");
-  });
-});
+describe('Pruebas en AddCategory', () => {
+    test('Debe cambiar el valor de la caja de texto', () => {
+        render(<AddCategory onNewCategory={() => {}} />)
+        const input = screen.getByRole('textbox')
+        fireEvent.input(input, { target: { value: 'Saitama' } }) // aqui es donde el usuario ingresa valores
+        expect(input.value).toBe('Saitama')
+    })
+
+    test('Debe de llamar onNewCategory si el input tiene un valor', () => {
+        const inputValue = 'Saitama'
+        // TODO: ???
+        render(<AddCategory onNewCategory={() => {}} />)
+        const input = screen.getByRole('textbox')
+        const form = screen.getByRole('form')
+
+        fireEvent.input(input, { target: { value: inputValue } })
+
+        // Ya establecemos el valor. Ahora queremos hacer el submit
+
+        fireEvent.submit(form)
+        expect(input.value).toBe('')
+    })
+})
